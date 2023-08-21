392ff961ae63762b6ff7745c6afed1dec92068f0
Israel Trujillo Domínguez <ingisraeltrujillo@gmail.com>
2023-08-20 22:41:59 -0500

    ES6

---
+++ bases/ES6/let-var.ts
@@ -0,0 +1,12 @@
+(()=> {
+
+  const nombre = 'Fernando';
+
+  const getName = (): void => {
+    console.log('Viejo getName');
+  }
+
+  // getName = () => {console.log('Nuevo getName')}
+  // getName()
+
+})()
